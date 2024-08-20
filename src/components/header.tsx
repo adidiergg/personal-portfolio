4d6d249ed2840f387bf15b4f77deab2ae7bed5ef
alanddgg <alandidiergon@gmail.com>
2024-08-19 21:24:23 -0600

    header styles

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,15 +8,12 @@ const fontSpeaceGrotesk = FontSpaceGrotesk({ subsets:["latin"]});
 
 export const Header = () => {
     return (
-        <header className={cn(fontSpeaceGrotesk.className,"fixed top-0 flex justify-center  lg:justify-end w-full  font-medium")}>
-
-            <nav className="flex flex-row">
-
-                <Link className="hover:bg-primary  px-6 py-3" href={"/"} > <span className="text-light text-lg">Home</span></Link>        
-                <Link className="hover:bg-primary  px-6 py-3"  href={"/about"} > <span className="text-light text-lg">About me</span></Link>
-                <Link className="hover:bg-primary px-6 py-3" href={"/projects"} > <span className="text-light text-lg">Projects</span></Link>
-                <Link className="hover:bg-primary px-6 py-3"  href={"/contact"} > <span className="text-light text-lg">Contact</span></Link>
-                
+        <header className={cn(fontSpeaceGrotesk.className,"fixed top-0 h-12 flex justify-center  lg:justify-end w-full  font-medium")}>
+            <nav className="flex flex-row justify-center items-center">
+                <Link className="px-1 lg:px-6" href={"/"} > <span className="text-light text-lg">Home</span></Link>        
+                <Link className="px-1  lg:px-6"  href={"/about"} > <span className="text-light text-lg">About me</span></Link>
+                <Link className="px-1 lg:px-6" href={"/projects"} > <span className="text-light text-lg">Projects</span></Link>
+                <Link className="px-1 lg:px-6"  href={"/contact"} > <span className="text-light text-lg">Contact</span></Link>
             </nav>
 
         </header>
